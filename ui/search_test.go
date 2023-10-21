f45a5406f7a76abc1ab354fb065e4b61c7c2dfde
Matteo Pacini <matteo@codecraft.it>
2023-10-21 16:51:00 +0100

    Moved header code, search view tests

---
+++ ui/search_test.go
@@ -0,0 +1,100 @@
+package ui
+
+import (
+	"testing"
+
+	"github.com/charmbracelet/bubbles/textarea"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSearchModel_Init(t *testing.T) {
+
+	t.Run("starts blinking the input field cursor", func(t *testing.T) {
+
+		model := NewSearchModel()
+
+		cmd := model.Init()
+		assert.NotNil(t, cmd)
+
+		var batchMsg tea.BatchMsg = cmd().(tea.BatchMsg)
+		found := false
+
+		for _, msg := range batchMsg {
+			currentMsg := msg()
+			if currentMsg == textarea.Blink() {
+				found = true
+				break
+			}
+		}
+
+		assert.True(t, found)
+
+	})
+
+	t.Run("broadcasts a bottomBarUpdateMsg", func(t *testing.T) {
+
+		model := NewSearchModel()
+
+		cmd := model.Init()
+		assert.NotNil(t, cmd)
+
+		var batchMsg tea.BatchMsg = cmd().(tea.BatchMsg)
+
+		found := false
+		var commands []string
+		for _, msg := range batchMsg {
+			currentMsg := msg()
+			if _, ok := currentMsg.(bottomBarUpdateMsg); ok {
+				commands = currentMsg.(bottomBarUpdateMsg).commands
+				found = true
+				break
+			}
+		}
+
+		assert.True(t, found)
+
+		expectedCommands := []string{"q: quit"}
+
+		assert.Equal(t, expectedCommands, commands)
+
+	})
+
+}
+
+func TestSearchModel_Update(t *testing.T) {
+
+	t.Run("broadcasts a quitMsg whe 'q' is pressed", func(t *testing.T) {
+
+		model := NewSearchModel()
+
+		input := tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune("q")}
+
+		_, cmd := model.Update(input)
+		assert.NotNil(t, cmd)
+
+		msg := cmd()
+
+		assert.IsType(t, quitMsg{}, msg)
+
+	})
+
+	t.Run("broadcasts a switchToLoadingModelMsg when 'enter' is pressed, propagating text area value", func(t *testing.T) {
+
+		model := NewSearchModel()
+		model.inputModel.SetValue("fancy value")
+
+		input := tea.KeyMsg{Type: tea.KeyEnter}
+
+		_, cmd := model.Update(input)
+		assert.NotNil(t, cmd)
+
+		msg := cmd()
+
+		assert.Equal(t, switchToLoadingModelMsg{
+			query: "fancy value",
+		}, msg)
+
+	})
+
+}
--- ui/styling.go
+++ ui/styling.go
@@ -20,9 +20,6 @@
 package ui
 
 import (
-	"fmt"
-	"radiogogo/data"
-
 	"github.com/charmbracelet/lipgloss"
 )
 
@@ -61,23 +58,4 @@ func StyleSetError(input string) string {
 	return lipgloss.NewStyle().
 		Foreground(lipgloss.Color("red")).
 		Render(input)
-}
-
-func Header() string {
-	headerStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("white")).
-		Background(lipgloss.Color("#5a4f9f")).
-		PaddingLeft(2).
-		PaddingRight(2)
-
-	versionStyle := lipgloss.NewStyle().
-		Foreground(lipgloss.Color("white")).
-		Background(lipgloss.Color("#8b77db")).
-		PaddingLeft(2).
-		PaddingRight(2)
-
-	header := headerStyle.Render("radiogogo")
-	version := versionStyle.Render(fmt.Sprintf("v%s", data.Version))
-
-	return header + version + "\n"
 }
